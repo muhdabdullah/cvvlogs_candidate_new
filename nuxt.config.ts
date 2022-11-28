0369fa0b13052deebd961c58a80fd98e7a538335
mowais20 <m.owais1045@gmail.com>
2022-11-28 18:55:24 +0500

    added poppins font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,15 +3,16 @@ export default defineNuxtConfig({
   build: {
     transpile: ["vuetify"],
   },
-  css: ["~/assets/css/main.css", "vuetify/lib/styles/main.sass"],
+  css: [
+    "~/assets/css/main.css",
+    "vuetify/lib/styles/main.sass",
+    "~/assets/fonts/Poppins.css",
+  ],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
-  modules: [
-    '@nuxt/image-edge',
-  ],
-  
+  modules: ["@nuxt/image-edge"],
 });
